6ceb8d0b9a134ea6f348aa37ec924c2f0de97acb
Mohammed <el-med7@outlook.com>
2020-12-30 11:33:55 +0100

    exo

---
+++ js/main.js
@@ -0,0 +1,59 @@
+let nombre = 8.56
+
+// 1
+// nombre = Math.round(8.56)
+// console.log(nombre)
+
+// 2
+
+// nombre = Math.round(8.56)
+// console.log(nombre)
+
+// 3
+
+// nombre = Math.floor(8.56)
+// console.log(nombre)
+
+// 4
+
+// nombre = Math.trunc(8.56)
+// console.log(nombre)
+
+// 5
+
+// nombre = Math.random()
+// console.log(nombre)
+
+// 6
+// let min = 1
+// let max = 100
+// nombre = Math.random() * (100 - 1) + 1;
+// console.log(nombre);
+
+// 7
+
+// nombre = Math.round(Math.random() * (100 - 1) + 1);
+// console.log(nombre);
+
+// 8
+
+// nombre = Math.pow(8, 2)
+// console.log(nombre)
+
+// 9
+
+// nombre = Math.sqrt(9);
+// console.log(nombre)
+
+// 10
+
+// nombre = Math.abs(1)
+// console.log(nombre)
+
+// 11
+// nombre = Math.max(-2, 1000, 8, 57)
+// console.log(nombre)
+
+// 12
+// nombre = Math.min(-2, 1000, 8, 57)
+// console.log(nombre)
